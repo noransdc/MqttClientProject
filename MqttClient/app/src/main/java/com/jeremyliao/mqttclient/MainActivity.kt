a8ed1b461b414a2874ac68d978e824a245a09ecf
Kyler <kyler@tbu.net>
2025-01-17 16:54:21 +0800

    first commit

---
+++ MqttClient/app/src/main/java/com/jeremyliao/mqttclient/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.jeremyliao.mqttclient
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity(){
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+
+}
